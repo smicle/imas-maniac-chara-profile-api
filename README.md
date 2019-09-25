69438ed5442eff9a0e680cf8535f2eb883a54ee8
smicle <bhhs1270026@gn.iwasaki.ac.jp>
2019-09-26 00:25:31 +0900

    create json-server API

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # imas-maniac-chara-profile-api
+
+json API: https://sleepy-gorge-86016.herokuapp.com/
---
+++ index.js
@@ -0,0 +1,7 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('./json/db.json')
+const middlewares = jsonServer.defaults()
+server.use(middlewares)
+server.use(router)
+server.listen(process.env.PORT || 5000, _ => console.log('JSON Server is running 5000'))
